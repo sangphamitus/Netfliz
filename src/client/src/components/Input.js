a3053a27f5a41f0434cd6bdfce2618b83692fc87
TinDaKing <tranhoangtin3702@gmail.com>
2022-11-21 22:36:36 +0700

    update input component

--- src/client/src/components/Input.js
+++ src/client/src/components/Input.js
@@ -1,16 +1,25 @@
 import React from "react";
-
-const Input = ({ inputTheme, labelText, labelTheme, containerTheme, placeHolder}) => {
+const Input = ({
+  inputTheme,
+  labelText,
+  labelTheme,
+  containerTheme,
+  placeHolder,
+}) => {
   return (
     <div className={`pt-8 grid grid-flow-row ${containerTheme}`}>
-      <label
-        className={`font-normal text-4xl font-button mb-2 text-gray-200 ${labelTheme}`}
-      >
-        {labelText}
-      </label>
-      <input className={`bg-gray-200 rounded-2xl h-16 w-auto ${inputTheme}`} placeholder= {placeHolder}/>
+      {labelText && (
+        <label
+          className={`font-normal text-4xl font-button mb-2 text-gray-200 ${labelTheme}`}
+        >
+          {labelText}
+        </label>
+      )}
+      <input
+        className={`rounded-2xl h-16 w-auto ${inputTheme} pl-4`}
+        placeholder={placeHolder}
+      />
     </div>
   );
 };
-
 export { Input };
