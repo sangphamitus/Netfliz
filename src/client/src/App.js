8ad6f92b8467aeb01276d737bc85102d5f832069
TinDaKing <tranhoangtin3702@gmail.com>
2022-11-21 23:37:08 +0700

    delete some routes, and move to pages

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,18 +1,11 @@
-// import logo from "./logo.svg";
-import { Button } from "./components/Button";
-import { NavBar } from "./components/NavBar";
-import { Footer } from "./components/Footer";
-import { Login } from "./routes/Login";
-import { Signup } from "./routes/Signup";
+import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div className="App bg-[#082032]">
-      <NavBar />
-      
-      <Login/>
-      <Signup/>
-      <Footer/>
+      {/* <SignUpPage /> */}
+      <SignUpPage />
     </div>
   );
 }
