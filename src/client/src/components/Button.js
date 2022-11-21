a4d7ce1e3fd46fbc7eacbae8d3b6152859e00f61
TinDaKing <tranhoangtin3702@gmail.com>
2022-11-21 23:35:24 +0700

    components updated

--- src/client/src/components/Button.js
+++ src/client/src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ children, props, theme }) => {
   return (
-    <button className={`w-36 h-16 rounded-3xl p-3 ${theme}`}>{children}</button>
+    <button className={`max-w-36 h-16 rounded-3xl p-3 ${theme}`}>{children}</button>
   );
 };
 
--- src/client/src/components/NavBar.js
+++ src/client/src/components/NavBar.js
@@ -21,7 +21,7 @@ const NavBar = () => {
     }
   }, [showLinks]);
   return (
-    <div className="nav-center flex flex-row bg-[rgb(0,0,0,0.75)]">
+    <div className="nav-center flex flex-row bg-black bg-opacity-75">
       <div className="nav-header px-6">
         <button className="nav-toggle hidden" onClick={toggleLinks}>
           <FontAwesomeIcon icon={faBars} />
@@ -39,25 +39,24 @@ const NavBar = () => {
       </div>
       <div className="flex mr-0 max-w-screen-md w-full ml-auto">
         <Input
-          inputTheme={"p-4 h-15 max-w-2xl w-auto bg-[rgb(0,0,0,0.75)]"}
+          inputTheme={"p-4 h-15 max-2w-xl w-auto bg-black bg-opacity-25"} 
           placeHolder={"Input movie name, category, actor"}
-          containerTheme={"pt-2 mb-2 w-full"}
-          className={""}
+          containerTheme={"pt-2 mb-2 w-full bg-opacity-25"}
         ></Input>
         <Button
           className="nav-toggle"
           onClick={toggleLinks}
-          theme="mt-2 ml-2 w-16 rounded-full"
+          theme="mt-2 w-14 rounded-full ml-0"
         >
           <FontAwesomeIcon icon={faMagnifyingGlass} inverse size="2x" />
         </Button>
       </div>
       <div className='mr-0 ml-auto'>
-        <Button theme={"bg-gray-200 my-2 mr-5"}>
-          <p className="text-5xl font-button text-[#CD0574]">LOGIN</p>
+        <Button theme={"bg-gray-200 my-2 mr-5 w-28 rounded-full"}>
+          <p className="text-4xl font-button text-[#CD0574]">LOGIN</p>
         </Button>
-        <Button theme={"bg-[#CD0574] my-2 mr-5"}>
-          <p className="text-5xl font-button text-gray-200">SIGNUP</p>
+        <Button theme={"bg-[#CD0574] my-2 mr-5 w-28 rounded-full"}>
+          <p className="text-4xl font-button text-gray-200">SIGNUP</p>
         </Button>
       </div>
     </div>
