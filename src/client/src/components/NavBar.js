fed6549cfbcbcd44369c51a923dcce93ed4d06f6
TinDaKing <tranhoangtin3702@gmail.com>
2022-11-21 22:36:09 +0700

    navbar with black search input

--- src/client/src/components/NavBar.js
+++ src/client/src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
   const [showLinks, setShowLinks] = useState(false);
   const linksContainerRef = useRef(null);
   const linksRef = useRef(null);
-  const toggleLinks = () => { 
+  const toggleLinks = () => {
     setShowLinks(!showLinks);
   };
   useEffect(() => {
@@ -17,44 +17,50 @@ const NavBar = () => {
     if (showLinks) {
       linksContainerRef.current.style.height = `${linksHeight}px`;
     } else {
-      linksContainerRef.current.style.height = '0px';
+      linksContainerRef.current.style.height = "0px";
     }
   }, [showLinks]);
   return (
-    <div className='nav-center flex flex-row bg-[rgb(0,0,0,0.75)]' >
-      <div className='nav-header pl-6'>
-        <button className='nav-toggle hidden' onClick={toggleLinks}>
-            <FontAwesomeIcon icon={faBars} />   
-
-          </button>
-          <a href="/"><img src={NetflizLogo} className='logo w-[160px]' alt='logo' /></a>
+    <div className="nav-center flex flex-row bg-[rgb(0,0,0,0.75)]">
+      <div className="nav-header px-6">
+        <button className="nav-toggle hidden" onClick={toggleLinks}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <a href="/">
+          <img
+            src={NetflizLogo}
+            className="logo w-[160px] max-w-none"
+            alt="logo"
+          />
+        </a>
       </div>
-      <div className='links-container' ref={linksContainerRef}>
-        <ul className='links' ref={linksRef}>
-        </ul>
-        
+      <div className="links-container" ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}></ul>
       </div>
-      <section className='flex mr-4 ml-auto'>
-        <Input inputTheme={'p-4 h-15 ml-12'} placeHolder={'Input movie name, category, actor'} containerTheme={'pt-0 w-[37rem] mb-2'}>
-
-        </Input>
-        <Button className='nav-toggle' onClick={toggleLinks} theme="mt-2 ml-2 w-16 rounded-full">
-            <FontAwesomeIcon icon={faMagnifyingGlass} inverse size="2x"/>   
+      <div className="flex mr-0 max-w-screen-md w-full ml-auto">
+        <Input
+          inputTheme={"p-4 h-15 max-w-2xl w-auto bg-[rgb(0,0,0,0.75)]"}
+          placeHolder={"Input movie name, category, actor"}
+          containerTheme={"pt-2 mb-2 w-full"}
+          className={""}
+        ></Input>
+        <Button
+          className="nav-toggle"
+          onClick={toggleLinks}
+          theme="mt-2 ml-2 w-16 rounded-full"
+        >
+          <FontAwesomeIcon icon={faMagnifyingGlass} inverse size="2x" />
         </Button>
-    
-      </section>
-      <Button theme={"bg-gray-200 my-2 mr-5"} >
-        <p className="text-5xl font-button text-[#CD0574]">LOGIN</p>
-      </Button>
-      <Button theme={"bg-[#CD0574] my-2 mr-5"}>
-        <p className="text-5xl font-button text-gray-200">SIGNUP</p>
-      </Button>
-
+      </div>
+      <div className='mr-0 ml-auto'>
+        <Button theme={"bg-gray-200 my-2 mr-5"}>
+          <p className="text-5xl font-button text-[#CD0574]">LOGIN</p>
+        </Button>
+        <Button theme={"bg-[#CD0574] my-2 mr-5"}>
+          <p className="text-5xl font-button text-gray-200">SIGNUP</p>
+        </Button>
+      </div>
     </div>
   );
 };
-
-
-
-export {NavBar};
-
+export { NavBar };
