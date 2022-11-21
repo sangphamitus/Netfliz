b2541203816717ab140d14418cb32a17ce24a028
TinDaKing <tranhoangtin3702@gmail.com>
2022-11-21 23:34:28 +0700

    log in and sign up new pages

---
+++ src/client/src/pages/SignUpPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+import { Text } from "../components/Text";
+import { Form } from "../components/Form"
+import { NavBar } from '../components/NavBar';
+
+function SignUpPage() {
+    return(
+      <div className="bg-local ... bg-login-background">
+        <NavBar/>
+        <div className='flex justify-center py-20'>
+        <Form formClass={'w-full'}>
+             <Text
+              customTheme="text-[3.5rem] text-pink-600 font-button"
+              isHeader={true}
+              text={"SIGNUP"}
+            />
+            <Input labelText={"EMAIL"}></Input>
+            <Input labelText={"USERNAME"}></Input>
+            <Input labelText={"PASSWORD"}></Input>
+            <Input labelText={"CONFIRM PASSWORD"}></Input>
+            <Button theme={"bg-pink-600 rounded-2xl w-full h-auto mt-20"}>
+              <Text
+                customTheme="text-[3.5rem] leading-none text-gray-200 font-button"
+                isHeader={false}
+                text="SIGNUP"
+              />
+            </Button>
+        </Form> 
+        </div>
+        
+      </div>
+    )
+  }
+  
+export default SignUpPage;
