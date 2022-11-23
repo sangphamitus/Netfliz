e8bcc08118b051a62b68c0bc25be30192f1bb39b
tkiet12 <kt97388@gmail.com>
2022-11-23 21:52:47 +0700

    move home page to pages

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,20 +1,8 @@
-// import logo from "./logo.svg";
-//import { Button } from "./components/Button";
-import { NavBar } from "./components/NavBar";
-import { Footer } from "./components/Footer";
-import { Banner } from "./components/Banner";
-import { ListMovies } from "./components/ListMovies";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App bg-[#082032]">
-      <NavBar />
-      <Banner />
-      
-      <ListMovies title={"NEW MOVIES"} />
-      <ListMovies title={"HOT MOVIES"} />
-      <Footer/>
-    </div>
+    <HomePage />
   );
 }
 
---
+++ src/client/src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavBar } from "../components/NavBar";
+import { Footer } from "../components/Footer";
+import { Banner } from "../components/Banner";
+import { ListMovies } from "../components/ListMovies";
+
+function HomePage() {
+    return (
+    <div className="App bg-[#082032]">
+        <NavBar />
+        <Banner />
+        <ListMovies title={"NEW MOVIES"} />
+        <ListMovies title={"HOT MOVIES"} />
+        <Footer/>
+    </div>
+    )
+}
+
+export default HomePage;
