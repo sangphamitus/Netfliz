72b6cf3b76f97efdf742029ba2d9285d983701dd
TinDaKing <tranhoangtin3702@gmail.com>
2022-11-28 22:02:47 +0700

    watch movie page release first time oh yeh

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,13 +2,15 @@ import HomePage from "./pages/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
+import WatchMoviePage from "./pages/WatchMoviePage";
+
 function App() {
   return (
   <div className="App bg-[#082032]">
     <BrowserRouter>
     <Routes>
       <Route exact path='/' element={<HomePage />} />
-      <Route path='/LogIn' element={<LogInPage />} />
+      <Route path='/LogIn' element={<WatchMoviePage/>} />
       <Route path='/SignUp' element={<SignUpPage />} />
     </Routes>
     </BrowserRouter>
