34c6efdce2b3c2d6032fc505d7e20567b9b3d046
tkiet12 <kt97388@gmail.com>
2022-12-12 15:36:23 +0700

    update movie info page

--- src/client/src/components/Rating.js
+++ src/client/src/components/Rating.js
@@ -1,32 +1,34 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { Text } from "./Text";
 
 const Rate = () => {
 	const [rate, setRate] = useState(0);
 	const [hover, setHover] = useState(null);
 
-	return (
-		<div className=" flex text-[20px]">
-		{[...Array(5)].map((star, index) => {
-			const Rating = index + 1;
-			return (
-			<label>
-				<button
-					type="button"
-					value={Rating}
-					onClick={() => {setRate(Rating)}}
-				/>
-				<div className=" cursor-pointer">
-					<FontAwesomeIcon icon={faStar}
-						color={Rating <= (hover || rate) ? "#FFFF00" : "#FFFFFF"}
-						onMouseEnter={() => {setHover(Rating)}}
-						onMouseLeave={() => {setHover(null)}}
+	return (	
+		<div className=" text-[20px] ">
+			<Text text={rate} isHeader={false} customTheme={"text-white pr-[5px]"}/>
+			{[...Array(5)].map((star, index) => {
+				const Rating = index + 1;
+				return (
+				<label>
+					<button
+						type="button"
+						value={Rating}
+						onClick={() => {setRate(Rating)}}
 					/>
-				</div>
-			</label>
-			);
-		})}
+					<div className=" cursor-pointer inline-flex">
+						<FontAwesomeIcon icon={faStar}
+							color={Rating <= (hover || rate) ? "#FFFF00" : "#FFFFFF"}
+							onMouseEnter={() => {setHover(Rating)}}
+							onMouseLeave={() => {setHover(null)}}
+						/>
+					</div>
+				</label>
+				);
+			})}
 		</div>
 	);
 };
--- src/client/src/pages/MovieInfoPage.js
+++ src/client/src/pages/MovieInfoPage.js
@@ -5,6 +5,9 @@ import { Text } from "../components/Text";
 import { NavBar } from '../components/NavBar';
 import { Footer} from '../components/Footer';
 import { Rate } from "../components/Rating";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
 
 function MovieInfoPage() {
    
@@ -16,19 +19,48 @@ function MovieInfoPage() {
                         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe>
                 <div className=" absolute top-[215px] left-[50px] z-[1]">
                     <Text text={"SMILE"} isHeader={true} customTheme={"text-6xl text-white"}/>
-                    <Rate />
-                    <p className="max-w-[250px] text-white font-normal">
-                    After witnessing a bizarre, traumatic incident involving a patient, 
-                    Dr. Rose Cotter starts experiencing frightening occurrences that 
-                    she can't explain. Rose must confront her troubling past in order to 
-                    survive and escape her horrifying new reality.
-                    </p> 
-
+                    <Button theme={"bg-[#D9D9D9] rounded-[3px] w-[150px] h-[60px] absolute top-[300px]"}>
+                        <FontAwesomeIcon icon={faPlay} size="2x"/>
+                        <Text
+                        customTheme="text-[40px] leading-none text-black-200 font-button"
+                        isHeader={false}
+                        text={"Watch"}
+                        />
+                    </Button>
+                    <Button theme={"bg-[#D9D9D9] rounded-[3px] w-[150px] h-[60px] absolute top-[300px] left-[170px]"}>
+                        <FontAwesomeIcon icon={faAdd} size="2x"/>
+                        <Text
+                        customTheme="text-[40px] leading-none text-black-200 font-button"
+                        isHeader={false}
+                        text={"List"}
+                        />
+                    </Button>
                 </div>
             </div>
-
-            <div className='bg-[#E5E5E5] w-auto min-h-[18rem] h-auto my-10 mx-5'>
-                <Text text={"Comments"} customTheme={"text-[2rem] px-5"} isHeader={true}/>
+            <div className=" inline-flex p-[20px]">
+                <Text text={"400 VIEWED"} isHeader={false} customTheme={"text-white text-[20px] pr-[20px]"}/>
+                <Rate />
+            </div>
+            <div className="flex p-[20px]">
+                <div>
+                <Text text={"2022 | Action | 1h 55m"} isHeader={false} customTheme={"text-white "}/>
+                <p className="max-w-[600px] text-white font-normal">
+                    Cười là phim điện ảnh kinh dị tâm lý Mỹ năm 2022 do Parker Finn biên kịch 
+                    và đạo diễn, dựa trên phim ngắn "Laura Has't Slept" năm 2020 của anh. Trong phim, 
+                    nữ diễn viên Sosie Bacon vào vai bác sĩ trị liệu Rose Cotter, cô gặp phải 
+                    những hiện tượng siêu nhiên kỳ lạ sau khi một bệnh nhân tự tử ngay tại phòng khám.
+                </p>
+                </div>
+                <div className=" flex flex-col">
+                <Text text={"Đạo diễn: Parker Finn"} isHeader={false} customTheme={"text-white ml-[100px]"}/>
+                <Text text={"Diễn Viên: Sosie Bacon, Kyle Gallner, ..."} isHeader={false} customTheme={"text-white ml-[100px]"}/>
+                </div>
+            </div>
+            <div>
+                <Text text={"EPISODES"} isHeader={true} customTheme={"text-[2rem] text-pink-600 font-button px-5"} />
+            </div>
+            <div className='w-auto min-h-[18rem] h-auto my-10 mx-5'>
+                <Text text={"Comments"} customTheme={"text-[2rem] px-5 text-white"} isHeader={true}/>
                 <div className='flex px-5 my-5'>
                     <Input containerTheme={"min-w-[38rem] pt-0"}/>
                     <Button theme={"bg-pink-600 rounded-2xl w-auto h-auto px-3 mx-3 px-4"}>
