9370e83e80fa5e777fe52682326502ad6c4c67be
ducquang2 <nducquangg@gmail.com>
2022-12-16 21:51:20 +0700

    new router

---
+++ src/client/src/modules.js
@@ -0,0 +1,17 @@
+import HomePage from "./pages/HomePage";
+import CategoriesPage from "./pages/CategoriesPage";
+import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
+import MovieInfoPage from "./pages/MovieInfoPage";
+import WatchMoviePage from "./pages/WatchMoviePage";
+import ResultPage from "./pages/ResultPage";
+
+export default [
+  HomePage,
+  CategoriesPage,
+  LogInPage,
+  SignUpPage,
+  MovieInfoPage,
+  WatchMoviePage,
+  ResultPage,
+];
