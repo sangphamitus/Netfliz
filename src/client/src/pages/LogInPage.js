e0402e553ef115736284c81abc3fe22d9b93a79d
Duc Quang <nducquangg@gmail.com>
2022-12-18 08:09:19 +0700

    Update LogInPage.js
    Nga`i sAng code diNk qUa

--- src/client/src/pages/LogInPage.js
+++ src/client/src/pages/LogInPage.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { Text } from "../components/Text";
-
 import { Form } from "../components/Form"
 import { NavBar } from '../components/NavBar';
 import { Footer} from '../components/Footer';
