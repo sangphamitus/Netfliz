82c3fea7bfa144a2858aaac89ff6344ce8a591f3
Pham Phuoc Sang <61650397+sangphamitus@users.noreply.github.com>
2022-12-21 18:41:42 +0700

    Update SignUpPage.js

--- src/client/src/pages/SignUpPage.js
+++ src/client/src/pages/SignUpPage.js
@@ -48,7 +48,7 @@ function SignUpPage() {
     <div className="bg-local ... bg-login-background">
       <NavBar allowSearch={false} />
       <div className="flex justify-center py-20">
-        <Form formClass={"w-[640px]"}>
+        <Form formClass={"w-full"}>
           <Text
             customTheme="text-[3.5rem] text-pink-600 font-button"
             isHeader={true}
