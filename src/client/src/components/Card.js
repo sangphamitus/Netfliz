a8409ecf59c37ee36a2ee3f6df449a03d4706b45
ducquang2 <nducquangg@gmail.com>
2022-12-22 22:00:34 +0700

    update css

--- src/client/src/components/Card.js
+++ src/client/src/components/Card.js
@@ -1,19 +1,34 @@
 import React from "react";
-import { Text } from "./Text";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const Card = ({ imgSrc,vid}) => {
-
-    const navigate=useNavigate();
-    const cardAccessing= ()=> {
-      navigate(`/info?vid=${vid}`)
-  
-    }
-    return (
-      <div className="bg-center bg-no-repeat overflow-hidden pr-6" onClick={(e)=>{cardAccessing()}}>
-          <img className="object-cover object-center h-fit w-96 " src={imgSrc} alt="movie-poster"/>
-      </div>
-    );
+const Card = ({ imgSrc, vid, canEdit }) => {
+  const navigate = useNavigate();
+  const cardAccessing = () => {
+    navigate(`/info?vid=${vid}`);
   };
-  
-  export { Card };
+  return (
+    <div
+      className="card bg-center bg-no-repeat overflow-hidden pr-6"
+      onClick={(e) => {
+        cardAccessing();
+      }}
+    >
+      <button className="trash-icon">
+        <FontAwesomeIcon
+          icon={faTrash}
+          className="cursor-pointer text-white hover:text-[#CD0574]"
+        />
+      </button>
+
+      <img
+        className="card-img object-cover object-center h-fit w-96"
+        src={imgSrc}
+        alt="movie-poster"
+      />
+    </div>
+  );
+};
+
+export { Card };
--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -1,14 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: true,
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      button: ["Bebas Neue", "cursive", "sans-serif"],
+    extend: {
+      fontFamily: {
+        button: ["Bebas Neue", "cursive", "sans-serif"],
+      },
+      backgroundImage: {
+        "login-background": "url('/src/assets/images/background_login.jpg')",
+      },
     },
-    backgroundImage: {
-      'login-background': "url('/src/assets/images/background_login.jpg')",
-    }
   },
-  plugins: [],
+  plugins: [theme()],
 };
+
+function theme() {
+  return plugin(function ({ addComponents, theme }) {
+    addComponents({
+      ...card(theme),
+    });
+  });
+}
+
+const card = (theme) => ({
+  ".card": {
+    position: "relative",
+    ".trash-icon": {
+      position: "absolute",
+      top: 20,
+      right: 50,
+      opacity: 0,
+    },
+  },
+  ".card:hover": {
+    ".trash-icon": {
+      opacity: 1,
+    },
+  },
+});
