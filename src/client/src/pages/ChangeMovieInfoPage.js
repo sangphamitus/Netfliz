038e8d4b194781fcecf3ead92a82c151236b2055
ducquang2 <nducquangg@gmail.com>
2022-12-28 21:13:20 +0700

    change info

--- src/client/src/pages/ChangeMovieInfoPage.js
+++ src/client/src/pages/ChangeMovieInfoPage.js
@@ -468,33 +468,30 @@ function ChangeMovieInfoPage() {
         </div>
       ) : (
         <div>
-          <div className="mx-36 py-14 h-auto">
-            <form
-              onSubmit={submit}
-              // method="post"
-              // action="/videos/search"
-              className="flex mr-10 max-w-screen-md w-full ml-auto"
+          <form
+            onSubmit={submit}
+            // method="post"
+            // action="/videos/search"
+            className="flex mr-10 max-w-screen-md w-full ml-auto"
+          >
+            <Input
+              inputTheme="p-4 h-10 max-2w-xl w-auto bg-black bg-opacity-25"
+              placeHolder="Input movie name or category"
+              containerTheme="pt-2 mb-2 w-full bg-opacity-25"
+              textColor="white"
+              name="name"
+              onChange={(e) => setTextInput(e.target.value)}
+            />
+            <Button
+              className="nav-toggle"
+              type="submit"
+              onClick={submit}
+              theme="w-14 rounded-full ml-0"
             >
-              <Input
-                inputTheme="p-4 h-10 max-2w-xl w-auto bg-black bg-opacity-25"
-                placeHolder="Input movie name or category"
-                containerTheme="pt-2 mb-2 w-full bg-opacity-25"
-                textColor="white"
-                name="name"
-                onChange={(e) => setTextInput(e.target.value)}
-              />
-              <Button
-                className="nav-toggle"
-                type="submit"
-                onClick={submit}
-                theme="w-14 rounded-full ml-0"
-              >
-                <FontAwesomeIcon icon={faMagnifyingGlass} inverse size="2x" />
-              </Button>
-            </form>
-            <div className="grid items-center justify-between grid-cols-3 grid-rows-4 my-5"></div>
-          </div>
-          <div className="grid items-center justify-between grid-cols-3 my-5">
+              <FontAwesomeIcon icon={faMagnifyingGlass} inverse size="2x" />
+            </Button>
+          </form>
+          <div className="grid items-center justify-items-center grid-cols-3 my-5 space-y-5">
             {movies &&
               movies.length > 0 &&
               movies.map((each, i) => {
